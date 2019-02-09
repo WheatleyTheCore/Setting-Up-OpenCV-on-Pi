7453108c07fc84af6331d6f9c98d0e7a16af44d2
WheatleyTheCore <alex.colwell@icloud.com>
2019-02-09 18:01:13 -0500

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,12 +1,12 @@
-sudo apt-get install build-essential cmake pkg-config &&
+sudo apt-get install build-essential cmake pkg-config -y &&
 echo "first batch completed"
-sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev &&
+sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev -y &&
 echo "second batch completed"
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev && 
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y && 
 echo "third batch completed"
-sudo apt-get install libxvidcore-dev libx264-dev &&
+sudo apt-get install libxvidcore-dev libx264-dev -y &&
 echo "fourth batch completed"
-sudo apt-get install libgtk2.0-dev libgtk-3-dev &&
+sudo apt-get install libgtk2.0-dev libgtk-3-dev -y &&
 echo "fifth batch completed"
-sudo apt-get install libatlas-base-dev gfortran &&
+sudo apt-get install libatlas-base-dev gfortran -y &&
 echo "all dependencies installed"
