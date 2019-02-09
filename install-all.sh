b2a324300371c3ca05e185e3d5b49f5c7d442cd0
WheatleyTheCore <alex.colwell@icloud.com>
2019-02-09 17:24:49 -0500

    Create install-all.sh

---
+++ install-all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+./install-dependancies &&
+
+./install-OpenCV $1 &&
+
+cd $PWD/opencv-$1
+mkdir build
+cd build
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-$1/modules \
+    -D BUILD_EXAMPLES=ON ..
+    
+if [ $2 ]
+then
+  make -j$2
+else
+  make
+fi
+
