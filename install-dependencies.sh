5103d520083877e479ce93acd48117baf0a3c833
WheatleyTheCore <alex.colwell@icloud.com>
2019-02-09 16:34:49 -0500

    created install dependencies script

---
+++ install-dependencies.sh
@@ -0,0 +1,12 @@
+sudo apt-get install build-essential cmake pkg-config &&
+echo "first batch completed"
+sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev &&
+echo "second batch completed"
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev && 
+echo "third batch completed"
+sudo apt-get install libxvidcore-dev libx264-dev &&
+echo "fourth batch completed"
+sudo apt-get install libgtk2.0-dev libgtk-3-dev &&
+echo "fifth batch completed"
+sudo apt-get install libatlas-base-dev gfortran 
+echo "all dependencies installed"
