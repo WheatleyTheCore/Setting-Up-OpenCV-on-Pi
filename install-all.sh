af5f9babdd1ca08951114d4232c71a79a86f8826
WheatleyTheCore <alex.colwell@icloud.com>
2019-02-12 16:21:31 -0500

    Update install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -8,10 +8,7 @@ cd $PWD/opencv-$1
 mkdir build
 cd build
 
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
-    -D CMAKE_INSTALL_PREFIX=/usr/local \
-    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-$1/modules \
-    -D BUILD_EXAMPLES=ON ..
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-$1/modules ..
     
 if [ $2 ]
 then
